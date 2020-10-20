79e707485c84b43f166dd577dc76b657a71ff3cc
munhunger <marcusmunger@gmail.com>
2020-10-20 20:26:39 +0200

    initial commit

---
+++ server/grafana.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+docker run \
+  -p 3000:3000 \
+  --name=grafana \
+  -e "GF_INSTALL_PLUGINS=grafana-clock-panel,grafana-simple-json-datasource" \
+  grafana/grafana
---
+++ server/src/main.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+const port = 5001;
+
+app.get("/", (req, res) => {
+  res.send("OK");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
